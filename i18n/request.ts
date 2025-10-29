abcf86f364e2d19d033c1ea319a130aef7bfaafc
mitoperni <mitoperni@gmail.com>
2025-10-29 09:24:11 +0100

    Fixed locale problem

--- i18n/request.ts
+++ i18n/request.ts
@@ -5,11 +5,17 @@ import { getRequestConfig } from 'next-intl/server';
 export const locales = ['es', 'en'] as const;
 export type Locale = typeof locales[number];
 
-export default getRequestConfig(async ({ locale }) => {
+export default getRequestConfig(async ({ requestLocale }) => {
+  // This typically corresponds to the `[locale]` segment
+  let locale = await requestLocale;
+
   // Validar que el locale exista
-  if (!locales.includes(locale as Locale)) notFound();
+  if (!locale || !locales.includes(locale as Locale)) {
+    notFound();
+  }
 
   return {
+    locale,
     messages: (await import(`../messages/${locale}.json`)).default
   };
 });
